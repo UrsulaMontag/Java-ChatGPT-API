7dced8641c5aa6d7b974a968024f489910a5770b
ugla <montagu666@gmail.com>
2024-06-21 10:52:31 +0200

    Initial commit

---
+++ src/main/java/um/javachatgptapi/JavaChatGptApiApplication.java
@@ -0,0 +1,13 @@
+package um.javachatgptapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaChatGptApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaChatGptApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/um/javachatgptapi/JavaChatGptApiApplicationTests.java
@@ -0,0 +1,13 @@
+package um.javachatgptapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaChatGptApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
